0e48f2ecece354874ce29456b7a39eeb8042dc5b
Eitan Elnekave <eitan02581@gmail.com>
2019-02-27 13:31:08 +0200

    nav ready

---
+++ js/nav-bar/cmp/nav-bar-apps-box.cmp.js
@@ -0,0 +1,10 @@
+export default {
+    template: `
+    <section class="apps-wrapper">
+        <div class="apps-container">
+            <button class="keep-app-btn"> <a>go to keep</a> </button>
+            <button class="email-app-btn"> <a>go to email</a> </button>
+        </div>
+    </section>
+    `
+}
--- js/nav-bar/cmp/nav-bar.cmp.js
+++ js/nav-bar/cmp/nav-bar.cmp.js
@@ -1,5 +1,41 @@
+import appsBox from './nav-bar-apps-box.cmp.js'
 export default {
+    components: {
+        appsBox
+    },
     template: `
-    <h1>asd</h1>
-    `
+    <section class="nav-wrapper">
+        <nav>
+           
+            <div class="logo-ham-container">
+                <div class="hamburger-wrapper">
+                    <button>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/VisualEditor_-_Icon_-_Menu.svg/768px-VisualEditor_-_Icon_-_Menu.svg.png">
+                    </button>
+                </div>
+                <div class="logo-wrapper">
+                    <img src="https://www.google.com/images/icons/product/keep-512.png"  >
+                </div>
+            </div>
+            
+            <div class="search-wrapper">
+                <input placeholder="Search">
+            </div>
+            <div class="apps-box-wrapper">
+                <button @click="showAppBox = !showAppBox">
+                    <i class="fas fa-th fa-2x"></i>
+                </button>
+            </div>
+        </nav>   
+        <appsBox v-if="showAppBox" />
+    </section>
+    `,
+    data() {
+        return {
+            showAppBox: false
+        }
+    },
+    methods: {
+
+    },
 }
